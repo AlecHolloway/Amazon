08415d6a98ea7c4d8c9d1c0818989d0d337fb61a
AlecHolloway <41766467+AlecHolloway@users.noreply.github.com>
2020-10-13 14:47:09 -0500

    Update inventory_class.py

--- HW3/inventory_class.py
+++ HW3/inventory_class.py
@@ -1,15 +1,24 @@
-from Database import *
+import sqlite3
+
+conn = sqlite3.connect('Users.db')
+
+c = conn.cursor()
 
 class Inventory():
 
-    def displayItems():
-        allItems()
+    def displayItems(self):
+        c.execute("SELECT Name, Description, Price FROM Inventory")
+        for row in c:
+            print(row)
+        return 0
 
     def addItems():
         pass
 
     def removeItems():
         pass
+
+
 
 
 
