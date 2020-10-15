f63044be6072ca499e08e2ab75c9b0548bad89e2
Monterius <56093638+Monterius@users.noreply.github.com>
2020-10-14 20:42:16 -0500

    Menu Overhaul & Remove From Cart
    Created main menu where you can decide what you want to do. Everything loops back to the main menu and you can exit from menu as well. Also you can remove an item from cart. If you already added an item, it will just update the quantity

--- HW3/Cart_class.py
+++ HW3/Cart_class.py
@@ -13,36 +13,37 @@ class Cart():
         self.unique_item_count = 0
         self.items = {} #dictionary used to store item and qty
 
-    def removeFromCart():
-        pass
+    def removeFromCart(self, item, amount):
+        if self.items.__contains__(item):
+            current = self.items.get(item)
+            updated = current - int(amount)
+            if updated < 0:
+                self.items[item] = 0
+            else:
+                self.items[item] = updated
+
+            print("Successfully removed %s %ss from cart!" % (amount, item))
+            self.showContents()
+
+        else:
+            print("Could not remove %s %ss from cart!" % (amount, item))
+        return 0
 
     def addToCart(self, item, amount):
         ##total_item_count += amount
-        self.unique_item_count += 1
+        if not self.items.__contains__(item):
+            self.items[item] = int(amount)
+            self.unique_item_count += 1
 
-        self.items[self.unique_item_count] = item
-        self.items["%d.qty" % self.unique_item_count] = int(amount)
+        else:
+            current = self.items.get(item)
+            self.items[item] = current + int(amount)
 
         print("Successfully added %s %ss to cart!" % (amount, item))
 
         self.showContents()
         return 0
-    
-    #def addToCart(self):
-    #    b = 1
-    #    cost = 0
-    #    current_item = None
-    #    while(b==1):
-    #       current_item = input("Select: ")
-    #       qty = int(input("How many?"))
-    #       items = {current_item, qty}
-    #       cost = queuePrice(current_item)
-    #       total_cost += qty * cost
-    #       b = input("Continue. 1 or 0")
            
-
-    
-       # return 0
 
     def showContents(self):
         for item in self.items.items():
--- HW3/main.py
+++ HW3/main.py
@@ -9,63 +9,99 @@ newCart = Cart()
 def main():
     print("Welcome to our store\n")
     choice = input("Login or Create Account 1 or 2: \n")
-    ##print(type(choice))
-    #print(choice)
-    
-    #while choice != "1" or choice != "2":
-      ##  choice = input("Please select 1 or 2: \n")
 
     if choice == "1":
-        n = 'y'
-        while(n == 'y'):
-            print("Enter Credentials\n")
-            userName = input("Enter username: \n")
-            password = input("Enter password: \n")
-            user = User(userName, password)
-            a = user.verify(userName, password)
-            if(a == True):
-                print("Sucessfully logged in")
-                n = 1
-            else:
-                print("Error logging in")
-                n = input("Try again? y/n") ## iterate until successful attempt
-
-        print("\n")
-        print("Welcome to the Store, our items are listed below:")
-        
-        ##inventory = Inventory()
-        inventory.displayItems() ## part d of assignment
-        print("\n")
-        print("\n")
-        print("Creating cart. Type one item at a time to add to cart")
-
-        WaitForUserToAddItem()
-
-
-
-
-
-                
+        login()
        
-
     if choice == "2":
-        print("Create new account\n")
-        userName = input("Enter username: ")
-        password = input("Enter password: ")
-        creditCard = input("Enter Credit Card Number: ")
-        address = input("Enter address: ")
+        createAccount()
 
-        newUser = User(userName, password)
-        newUser.addUsertoDatabase(userName, password, creditCard, address)
+    print("\n")
+    print("Welcome to the Store, our items are listed below:")
+        
+    inventory.displayItems() ## part d of assignment
+    print("\n")
+    print("\n")
+    print("Creating cart. Type one item at a time to add to cart")
+
+    WaitForUserToChooseOption()
+
+def login():
+    n = 'y'
+    while(n == 'y'):
+        print("Enter Credentials\n")
+        userName = input("Enter username: \n")
+        password = input("Enter password: \n")
+        user = User(userName, password)
+        a = user.verify(userName, password)
+        if(a == True):
+            print("Sucessfully logged in")
+            n = 1
+        else:
+            print("Error logging in")
+            n = input("Try again? y/n") ## iterate until successful attempt
+
+def createAccount():
+    print("Create new account\n")
+    userName = input("Enter username: ")
+    password = input("Enter password: ")
+    creditCard = input("Enter Credit Card Number: ")
+    address = input("Enter address: ")
+
+    newUser = User(userName, password)
+    newUser.addUsertoDatabase(userName, password, creditCard, address)
         
 
-def WaitForUserToAddItem():
+def WaitForUserToAddOrRemoveItem(choice):
+    inventory.displayItems()
+
+    if choice == 1:
         item = input("Name of item: ")
         if inventory.VerifyItemIsInDatabase(item):
             qty = input("How many? ")
             print("Adding %s %ss to cart..." % (qty , item))
             newCart.addToCart(item, qty)
 
-        WaitForUserToAddItem()
+        else:
+            print("Could not find item in database!\n")
+            WaitForUserToChooseOption()
+
+    elif choice == 2:
+        item = input("Name of item: ")
+        if inventory.VerifyItemIsInDatabase(item):
+            qty = input("How many? ")
+            print("Removing %s %ss from cart..." % (qty , item))
+            newCart.removeFromCart(item, qty)
+
+        else:
+            print("Could not find item in database!\n")
+            WaitForUserToChooseOption()
+
+def WaitForUserToChooseOption():
+    print("1: Add items to cart\n2: Remove items from cart\n3: View cart\n4: Checkout\n5: Exit")
+    try:
+        option = int(input("What would you like to do? "))
+    except:
+        print("Incorrect input. Please try again\n")
+        WaitForUserToChooseOption()
+
+    if option == 1 or option == 2:
+        WaitForUserToAddOrRemoveItem(option)
+        WaitForUserToChooseOption()
+
+    elif option == 3:
+        newCart.showContents()
+        WaitForUserToChooseOption()
+
+    elif option == 4:
+        WaitForUserToChooseOption()
+
+    elif option == 5:
+        exit()
+
+    else:
+        print("Incorrect input. Please try again\n")
+        WaitForUserToChooseOption()
+
 
 main()
